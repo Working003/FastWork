b3aca77736f721fc932ceb254a4e5e6afa908253
João Pedro Netto Carvalho <joaopedronettocarvalho@gmail.com>
2025-06-16 14:15:17 -0300

    adição do fastwork

---
+++ fastwork-banco-main/fastwork-banco-main/models/tarefa.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+
+@dataclass
+
+class Tarefa:    
+        id: int
+        descricao: str
+        empregador: str
+        avaliacao: str
+        endereco: str
+        valor: float
+        data: str
+        status: str
---
+++ fastwork-banco-main/fastwork-banco-main/models/usuario.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+
+@dataclass
+
+class Usuario:
+    id: int
+    nome: str
+    email: str
+    senha: str
+    telefone: str
+    data_nascimento: str
+    habilidade: str
